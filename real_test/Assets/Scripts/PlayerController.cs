a2626e5e60470ccf44338aa7f7624a0b400cbec8
victor890915 <victor890915@gmail.com>
2022-06-24 17:59:16 +0800

    fixed gravity issues

--- real_test/Assets/Scripts/PlayerController.cs
+++ real_test/Assets/Scripts/PlayerController.cs
@@ -9,19 +9,34 @@ public class PlayerController : MonoBehaviour
     public SteamVR_Action_Vector2 controllerInput;
     private CharacterController characterController;
     public float moveSpeed = 5f;
+    private int onGround = 0;
+    public int playerHehght;
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
     }
     void Update()
     {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit raycastHit;
+        if(Physics.Raycast(ray, out raycastHit))
+        {
+            if(raycastHit.distance<5)
+            {
+                onGround = 0;
+            }
+            else
+            {
+                onGround = 1;
+            }
+        }
+        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(controllerInput.axis.x, 0, controllerInput.axis.y));
+        characterController.Move(moveSpeed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up)- onGround *new Vector3(0,9.81f,0)*Time.deltaTime);
+        /*
         if (controllerInput.axis.magnitude > 0.05f)
         {
-            Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(controllerInput.axis.x, 0, controllerInput.axis.y));
-            characterController.Move(moveSpeed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up)-new Vector3(0,9.81f,0)*Time.deltaTime);
-            //characterController.Move(moveSpeed * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up));
-
         }
+        */
 
     }
 }

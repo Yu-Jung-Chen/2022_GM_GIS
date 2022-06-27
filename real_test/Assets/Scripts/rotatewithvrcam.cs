c734716c0d650a7a3f814a0273f758772725dc37
Lin Chen <dhps60111@gmail.com>
2022-06-27 17:04:16 +0800

    angle change test

--- real_test/Assets/Scripts/rotatewithvrcam.cs
+++ real_test/Assets/Scripts/rotatewithvrcam.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class rotatewithvrcam : MonoBehaviour
 {
-    GameObject VRcamera =GameObject
+    public Transform mapCam;
+
+    public Vector3 vrCamAngle;
+    public Vector3 mapCamAngle;
+
+    public GameObject VRCamera;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        VRCamera = GameObject.Find("VRCamera");
     }
 
     // Update is called once per frame

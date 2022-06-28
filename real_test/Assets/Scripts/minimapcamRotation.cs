59547459c7341dc0563e41057e5513ba1a23f08e
Lin Chen <dhps60111@gmail.com>
2022-06-28 11:31:03 +0800

    minimap camera

--- real_test/Assets/Scripts/minimapcamRotation.cs
+++ real_test/Assets/Scripts/minimapcamRotation.cs
@@ -5,19 +5,11 @@ using UnityEngine;
 public class minimapcamRotation : MonoBehaviour
 {
     public Transform VRCamera;
-    public GameObject minimapCamera;
     public Transform minimapCamTrans;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        minimapCamTrans = minimapCamera.transform;
-    }
 
     // Update is called once per frame
     void Update()
     {
-        minimapCamTrans.localEulerAngles.x = -VRCamera.eulerAngles.x;
-        minimapCamTrans.localEulerAngles.z = -VRCamera.eulerAngles.z;
+        minimapCamTrans.Rotate(-VRCamera.eulerAngles.x,VRCamera.localEulerAngles.y,-VRCamera.eulerAngles.z);
     }
 }

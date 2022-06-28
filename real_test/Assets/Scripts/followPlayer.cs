300e4e928cc965eb52cb340c376cd1990d6e6502
victor890915 <victor890915@gmail.com>
2022-06-28 17:05:35 +0800

    really fixed minimap rotation

--- real_test/Assets/Scripts/followPlayer.cs
+++ real_test/Assets/Scripts/followPlayer.cs
@@ -19,9 +19,17 @@ public class followPlayer : MonoBehaviour
     {
         
         transform.position = followWho.transform.position;
-        turnAngel= followWho.transform.rotation.y + vrCam.transform.rotation.y;
+        //turnAngel= (followWho.transform.rotation.eulerAngles.y + vrCam.transform.localRotation.eulerAngles.y)%360;
+        
+        transform.rotation = Quaternion.Euler(0,vrCam.transform.rotation.eulerAngles.y, 0);
 
-        transform.rotation = Quaternion.EulerAngles(0, turnAngel, 0);
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log($"turnAngle= {turnAngel}");
+            Debug.Log($"player transform rotation = {followWho.transform.rotation.eulerAngles.y}");
+            Debug.Log($"vrcam transform rotation = {vrCam.transform.rotation.eulerAngles.y}");
+        }
+
         
     }
 }

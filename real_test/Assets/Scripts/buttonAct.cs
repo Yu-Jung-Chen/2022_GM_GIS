3ce8a02260a7a844ed9f9408804601a43d7af3f9
Lin Chen <dhps60111@gmail.com>
2022-06-29 16:15:51 +0800

    map change

--- real_test/Assets/Scripts/buttonAct.cs
+++ real_test/Assets/Scripts/buttonAct.cs
@@ -17,16 +17,46 @@ public class buttonAct : MonoBehaviour
     public GameObject WorldMap;
     public GameObject Buttons;
 
+    Transform CamTransform;
+
     private void Awake()
     {
         BigCam = GameObject.Find("worldCam");
         BigCamIcon = GameObject.Find("minimapCamAndIcon/bigmapicon");
         WorldMap = GameObject.Find("Canvas/map");
         Buttons = GameObject.Find("Canvas/worldmap");
+        CamTransform = BigCam.transform;
     }
 
     public void camUp()
     {
+        CamTransform.position = new Vector3(BigCam.transform.position.x + 50, BigCam.transform.position.y, BigCam.transform.position.z);
+        cameraplace[0]++;
+    }
+    public void camDown()
+    {
+        CamTransform.position = new Vector3(BigCam.transform.position.x - 50, BigCam.transform.position.y, BigCam.transform.position.z);
+        cameraplace[0]--;
+    }
+    public void camLeft()
+    {
+        CamTransform.position = new Vector3(BigCam.transform.position.x, BigCam.transform.position.y, BigCam.transform.position.z + 50);
+        cameraplace[1]++;
+    }
+    public void camRight()
+    {
+        CamTransform.position = new Vector3(BigCam.transform.position.x, BigCam.transform.position.y, BigCam.transform.position.z - 50);
+        cameraplace[1]--;
+    }
 
+    public void plus()
+    {
+        BigCam.GetComponent<Camera>().orthographicSize -= 50;
+        BigCamIcon.transform.localScale -=new Vector3(5,5,0);
+    }
+    public void minus()
+    {
+        BigCam.GetComponent<Camera>().orthographicSize += 50;
+        BigCamIcon.transform.localScale += new Vector3(5, 5, 0);
     }
 }

f7252d3d483d89fef1ef0ce20b973a7b448aa8cd
Lin Chen <dhps60111@gmail.com>
2022-06-30 15:59:31 +0800

    click

--- real_test/Assets/Scripts/SceneHandler.cs
+++ real_test/Assets/Scripts/SceneHandler.cs
@@ -10,6 +10,8 @@ public class SceneHandler : MonoBehaviour
 {
     public SteamVR_LaserPointer laserPointer;
 
+    private PointerEventData m_data=null;
+
     void Awake()
     {
         laserPointer.PointerIn += PointerInside;
@@ -21,24 +23,29 @@ public class SceneHandler : MonoBehaviour
     {
         if (e.target.GetComponent<Button>() !=null )
         {
+            var button = e.target.GetComponent<Button>();
             Debug.Log("clicked button");
+            ExecuteEvents.Execute(EventSystem.current.currentSelectedGameObject, new PointerEventData(EventSystem.current), ExecuteEvents.submitHandler);
         }
     }
-
+    
     public void PointerInside(object sender, PointerEventArgs e)
     {
-
+        if (e.target.GetComponent<Button>() != null)
+        {
+            var button = e.target.GetComponent<Button>();
+            Debug.Log("enter button");
+            button.Select();
+        }
     }
 
     public void PointerOutside(object sender, PointerEventArgs e)
     {
-        if (e.target.name == "up")
+        if (e.target.GetComponent<Button>() != null)
         {
-            Debug.Log("Cube was exited");
-        }
-        else if (e.target.name == "Button")
-        {
-            Debug.Log("Button was exited");
+            var button = e.target.GetComponent<Button>();
+            Debug.Log("leave button");
+            EventSystem.current.SetSelectedGameObject(null);
         }
     }
 }
